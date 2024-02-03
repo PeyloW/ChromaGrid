04e54a81a5fe67b7c826ae122eae06dfb9e3b4e1
PeyloW <peylow@gmail.com>
2024-02-03 10:13:06 -0800

    Initial Commit

---
+++ ChromaGrid/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ChromaGrid
+//
+//  Created by Fredrik on 2024-02-03.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ChromaGrid/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ChromaGrid
+//
+//  Created by Fredrik on 2024-02-03.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
