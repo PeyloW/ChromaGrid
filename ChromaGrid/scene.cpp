a3a54b3c90686593f9fc42f8c33b7eba210e776f
PeyloW <peylow@gmail.com>
2024-03-01 20:00:22 -0800

    Double buffering, and better transition state.

--- ChromaGrid/scene.cpp
+++ ChromaGrid/scene.cpp
@@ -11,10 +11,16 @@ cgmanager_c::cgmanager_c() :
     _super_token(cgsuper(0)),
     vbl(cgtimer_c::vbl),
     mouse((cgrect_t){{0,0}, {320, 192}}),
-    _physical_screen((cgsize_t){320, 208}, false, nullptr),
+    _physical_screen_0((cgsize_t){320, 208}, false, nullptr),
+    _physical_screen_1((cgsize_t){320, 208}, false, nullptr),
     _logical_screen((cgsize_t){320, 208}, false, nullptr)
 {
-    _dirtymap = (bool *)calloc(_physical_screen.dirtymap_size(), 1);
+    _active_physical_screen = 0;
+    _scene_count = 0;
+    _delete_count = 0;
+    _transition_state.full_restores_left = 0;
+    _dirtymap_0 = (bool *)calloc(_physical_screen_0.dirtymap_size(), 1);
+    _dirtymap_1 = (bool *)calloc(_physical_screen_1.dirtymap_size(), 1);
 }
 
 cgmanager_c::~cgmanager_c() {
@@ -27,7 +33,6 @@ cgmanager_c::~cgmanager_c() {
 void cgmanager_c::run(cgscene_c *rootscene) {
 #if !DEBUG_NO_SET_SCREEN
     int16_t old_mode = cgset_screen(nullptr, nullptr, 0);
-    _physical_screen.set_active();
 #endif
 
     push(rootscene);
@@ -42,42 +47,45 @@ void cgmanager_c::run(cgscene_c *rootscene) {
 #endif
     vbl.reset_tick();
     while (_scene_count > 0) {
-        if (_transition_shade > 0) {
+        auto &physical_screen = (_active_physical_screen == 0) ? _physical_screen_0 : _physical_screen_1;
+        auto &dirtymap = (_active_physical_screen == 0) ? _dirtymap_0 : _dirtymap_1;
+        if (_transition_state.full_restores_left > 0) {
 #if DEBUG_COLORS
             color_in_transition.set_at(0);
 #endif
-            _physical_screen.with_stencil(cgimage_c::get_stencil(_transion_type, cgimage_c::STENCIL_FULLY_OPAQUE - _transition_shade), [&, this] {
+            if (_transition_state.type == cgimage_c::none) {
+                physical_screen.draw_aligned(_logical_screen, (cgpoint_t){0,0});
+                _transition_state.full_restores_left--;
+            } else {
                 auto old_tick = vbl.tick();
-                _physical_screen.draw_aligned(_logical_screen, (cgpoint_t){0, 0});
+                auto shade = MIN(cgimage_c::STENCIL_FULLY_OPAQUE, _transition_state.shade);
+                physical_screen.with_stencil(cgimage_c::get_stencil(_transition_state.type, shade), [this, &physical_screen] {
+                    physical_screen.draw_aligned(_logical_screen, (cgpoint_t){0, 0});
+                });
+                if (shade == cgimage_c::STENCIL_FULLY_OPAQUE) {
+                    _transition_state.full_restores_left--;
+                }
                 auto ticks = MAX(1, vbl.tick() - old_tick);
-#if DEBUG_NO_SET_SCREEN
-                printf("Transition (%d) %d ticks.\n\r", old_tick, ticks);
-#endif
-                _transition_shade -= ticks;
-            });
+                _transition_state.shade += ticks;
+            }
         } else {
             if (_scene_count > 1) {
 #if DEBUG_COLORS
             color_in_top_tick.set_at(0);
 #endif
-                _logical_screen.with_dirtymap(_dirtymap, [this] {
+                _logical_screen.with_dirtymap(dirtymap, [this] {
                     top_scene().tick(_logical_screen);
                 });
             }
 #if DEBUG_COLORS
             color_in_restore.set_at(0);
 #endif
-            if (_full_restore) {
-                _physical_screen.draw_aligned(_logical_screen, (cgpoint_t){0, 0});
-                _full_restore = false;
-            } else {
-                _physical_screen.restore(_logical_screen, _dirtymap);
-            }
+            physical_screen.restore(_logical_screen, dirtymap);
 #if DEBUG_COLORS
             color_in_root_tick.set_at(0);
 #endif
-            _physical_screen.with_dirtymap(_dirtymap, [this] {
-                _scene_stack[0]->tick(_physical_screen);
+            physical_screen.with_dirtymap(dirtymap, [this, &physical_screen] {
+                _scene_stack[0]->tick(physical_screen);
             });
             while (_delete_count-- > 0) {
                 delete _deletion_stack[_delete_count];
@@ -86,6 +94,10 @@ void cgmanager_c::run(cgscene_c *rootscene) {
 #if DEBUG_COLORS
             color_done.set_at(0);
 #endif
+        cgtimer_c::with_paused_timers([this, &physical_screen] {
+            physical_screen.set_active();
+            _active_physical_screen = (_active_physical_screen + 1) & 0x1;
+        });
         vbl.wait();
     }
 #if !DEBUG_NO_SET_SCREEN
@@ -101,9 +113,7 @@ void cgmanager_c::push(cgscene_c *scene, cgimage_c::stencil_type_e transition) {
     _logical_screen.with_dirtymap(nullptr, [this] {
         top_scene().will_appear(_logical_screen, false);
     });
-    _full_restore = true;
-    _transion_type = transition;
-    _transition_shade = transition != cgimage_c::none ? cgimage_c::STENCIL_FULLY_OPAQUE : 0;
+    enqueue_transition(transition);
 }
 
 void cgmanager_c::pop(cgimage_c::stencil_type_e transition, int count) {
@@ -115,9 +125,7 @@ void cgmanager_c::pop(cgimage_c::stencil_type_e transition, int count) {
     _logical_screen.with_dirtymap(nullptr, [this, count] {
         top_scene().will_appear(_logical_screen, true);
     });
-    _full_restore = true;
-    _transion_type = transition;
-    _transition_shade = transition != cgimage_c::none ? cgimage_c::STENCIL_FULLY_OPAQUE : 0;
+    enqueue_transition(transition);
 }
 
 void cgmanager_c::replace(cgscene_c *scene, cgimage_c::stencil_type_e transition) {
@@ -127,8 +135,6 @@ void cgmanager_c::replace(cgscene_c *scene, cgimage_c::stencil_type_e transition
     _logical_screen.with_dirtymap(nullptr, [this] {
         top_scene().will_appear(_logical_screen, false);
     });
-    _full_restore = true;
-    _transion_type = transition;
-    _transition_shade = transition != cgimage_c::none ? cgimage_c::STENCIL_FULLY_OPAQUE : 0;
+    enqueue_transition(transition);
 }
 
--- ChromaGrid/scene.hpp
+++ ChromaGrid/scene.hpp
@@ -48,20 +48,31 @@ public:
     cgimage_c &get_logical_screen() { return _logical_screen; }
     
 private:
-    inline void enqueue_delete(cgscene_c *scene) {
-        _deletion_stack[_delete_count++] = scene;
-    }
     int _scene_count;
     cgscene_c *_scene_stack[8];
     int _delete_count;
     cgscene_c *_deletion_stack[8];
+    inline void enqueue_delete(cgscene_c *scene) {
+        _deletion_stack[_delete_count++] = scene;
+    }
 
-    cgimage_c _physical_screen;
+    cgimage_c _physical_screen_0;
+    bool *_dirtymap_0;
+    cgimage_c _physical_screen_1;
+    bool *_dirtymap_1;
+    int _active_physical_screen;
     cgimage_c _logical_screen;
-    bool *_dirtymap;
-    bool _full_restore;
-    cgimage_c::stencil_type_e _transion_type;
-    int _transition_shade;
+
+    struct {
+        int full_restores_left;
+        cgimage_c::stencil_type_e type;
+        int shade;
+    } _transition_state;
+    inline void enqueue_transition(cgimage_c::stencil_type_e type) {
+        _transition_state.full_restores_left = 2;
+        _transition_state.type = type;
+        _transition_state.shade = 0;
+    }
 };
 
 #endif /* scene_hpp */
